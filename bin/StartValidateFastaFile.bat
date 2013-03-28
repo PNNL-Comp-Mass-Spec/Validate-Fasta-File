20f37481501c1352237b0c4136345b9575f624a5
Matthew Monroe <matthew.monroe@pnnl.gov>
2013-03-28 03:13:58 +0000

    Removed binaries from SVN

--- bin/StartValidateFastaFile.bat
+++ bin/StartValidateFastaFile.bat
@@ -1,2 +1,4 @@
 rem ValidateFastaFile.exe /i:\\gigasax\dms_organism_Files\*.fasta /p:ValidateFastaFileOptions.xml /c /s:2
-ValidateFastaFile.exe /i:JunkTest.fasta /p:ValidateFastaFile_ModelSettings.xml
+rem ValidateFastaFile.exe JunkTest.fasta /P:ValidateFastaFile_ModelSettings.xml
+ValidateFastaFile.exe QC_Standards_2004-01-21_Dup.fasta /P:ValidateFastaFile_CreatedFixedFasta.xml
+
