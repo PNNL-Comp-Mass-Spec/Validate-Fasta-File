5cb326ffe769476eeadb93289ba7522bcc8e4e1d
Matthew Monroe <matthew.monroe@pnnl.gov>
2014-08-25 20:52:23 +0000

    Add batch file

---
+++ bin/DLL/Distribute_DLL.bat
@@ -0,0 +1,12 @@
+@echo off
+
+Copy ValidateFastaFile.dll "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\ValidateFastaFile.dll"
+Copy ValidateFastaFile.dll "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\DLL\ValidateFastaFile.dll"
+Copy ValidateFastaFile.dll "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\Lib\ValidateFastaFile.dll"
+Copy ValidateFastaFile.dll "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Debug\ValidateFastaFile.dll"
+Copy ValidateFastaFile.dll "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Release\ValidateFastaFile.dll"
+Copy ValidateFastaFile.dll "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Protein_Uploader\bin\ValidateFastaFile.dll"
+Copy ValidateFastaFile.dll "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Shared Libraries\ValidateFastaFile.dll"
+Copy ValidateFastaFile.dll "F:\My Documents\Projects\DataMining\Validate_Fasta_File\TestValidateFastaFileDLL\bin\ValidateFastaFile.dll"
+
+pause
