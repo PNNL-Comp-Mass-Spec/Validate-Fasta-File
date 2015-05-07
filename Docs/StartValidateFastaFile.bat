a8063cd6a04e47382f0e7ca37aab107227e1c0f7
alchemistmatt <matthew.monroe@pnnl.gov>
2015-05-07 14:56:17 -0700

    Add batch file and example fasta files

--- Docs/StartValidateFastaFile.bat
+++ Docs/StartValidateFastaFile.bat
@@ -1,4 +1,4 @@
 rem ValidateFastaFile.exe /i:\\gigasax\dms_organism_Files\*.fasta /p:ValidateFastaFileOptions.xml /c /s:2
 rem ValidateFastaFile.exe JunkTest.fasta /P:ValidateFastaFile_ModelSettings.xml
-ValidateFastaFile.exe QC_Standards_2004-01-21_Dup.fasta /P:ValidateFastaFile_CreatedFixedFasta.xml
+..\bin\ValidateFastaFile.exe QC_Standards_2004-01-21_Dup.fasta /P:ValidateFastaFileOptions_GenerateFixedFasta.xml
 
