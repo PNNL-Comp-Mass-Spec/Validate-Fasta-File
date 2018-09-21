528c7a14e875de4f499aff146b18c7bca29b0ae0
alchemistmatt <matthew.monroe@pnnl.gov>
2018-09-20 18:45:40 -0700

    Update batch file

--- bin/DLL/Distribute_DLL.bat
+++ bin/DLL/Distribute_DLL.bat
@@ -1,13 +1,13 @@
 @echo off
 
-call Distribute_DLL_Work.bat "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\"
-call Distribute_DLL_Work.bat "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\DLL\"
-call Distribute_DLL_Work.bat "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\Lib\"
+call Distribute_DLL_Work.bat "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\"
+call Distribute_DLL_Work.bat "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\DLL\"
+call Distribute_DLL_Work.bat "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\Lib\"
 
-call Distribute_DLL_Work.bat "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Debug\"
-call Distribute_DLL_Work.bat "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Release\"
-call Distribute_DLL_Work.bat "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Protein_Uploader\bin\"
-call Distribute_DLL_Work.bat "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Shared Libraries\"
-call Distribute_DLL_Work.bat "F:\My Documents\Projects\DataMining\Validate_Fasta_File\TestValidateFastaFileDLL\bin\"
+call Distribute_DLL_Work.bat "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Debug\"
+call Distribute_DLL_Work.bat "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Release\"
+call Distribute_DLL_Work.bat "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Protein_Uploader\bin\"
+call Distribute_DLL_Work.bat "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Shared Libraries\"
+call Distribute_DLL_Work.bat "F:\Documents\Projects\DataMining\Validate_Fasta_File\TestValidateFastaFileDLL\bin\"
 
 pause
