e90d7083e9ccc6c7bfb617cb62a9bb8ec9a68362
alchemistmatt <matthew.monroe@pnnl.gov>
2020-04-15 15:50:25 -0700

    Add batch file and example output files

---
+++ Docs/ProcessFiles.bat
@@ -0,0 +1,5 @@
+..\bin\ValidateFastaFile.exe JunkTest.fasta /F /R
+
+..\bin\ValidateFastaFile.exe Shewanella_2003-12-19.fasta /F
+
+..\bin\ValidateFastaFile.exe QC_Standards_2004-01-21_Dup.fasta /P:ValidateFastaFileOptions_GenerateFixedFasta.xml
