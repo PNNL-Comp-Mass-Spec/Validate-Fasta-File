60bbfd31d9ba3a5cf25f024b3a1032ab10092a4e
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2020-04-27 16:22:07 -0700

    Update csproj, AssemblyInfo, and App.config files
    Give the DLL a different intermediate and binary output path

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,32 +1,37 @@
-﻿Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-' General Information about an assembly is controlled through the following
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
 
-' Review the values of the assembly attributes
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
 
-<Assembly: AssemblyTitle("")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("")>
-<Assembly: AssemblyCopyright("")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: CLSCompliant(True)>
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("57B1431F-B97B-4014-99BC-04AAE5FCEB07")]
 
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("57B1431F-B97B-4014-99BC-04AAE5FCEB07")>
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers
-' by using the '*' as shown below:
-
-<Assembly: AssemblyVersion("2.2.*")>
+[assembly: AssemblyVersion("2.2.*")]
