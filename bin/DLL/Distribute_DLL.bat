795822b3937d0bdbb248ace420ddbf7df385d7bc
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-05 14:59:18 -0700

    Update batch files

--- bin/DLL/Distribute_DLL.bat
+++ bin/DLL/Distribute_DLL.bat
@@ -8,6 +8,7 @@ call Distribute_DLL_Work.bat F:\Documents\Projects\KenAuberry\Organism_Database_
 call Distribute_DLL_Work.bat F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Release\
 call Distribute_DLL_Work.bat F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Protein_Uploader\bin\
 call Distribute_DLL_Work.bat F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Lib\
+
 call Distribute_DLL_Work.bat F:\Documents\Projects\DataMining\Validate_Fasta_File\TestValidateFastaFileDLL\bin\
 
 pause
--- bin/DLL/Distribute_DLL_Work.bat
+++ bin/DLL/Distribute_DLL_Work.bat
@@ -1,7 +1,9 @@
 @echo off
 
 echo Copying to %1
-xcopy ValidateFastaFile.dll %1 /d /Y
+xcopy ValidateFastaFile.dll %1 /d /y
+xcopy FlexibleFileSortUtility.dll %1 /d /y
 
-if exist %1\ValidateFastaFile.pdb (xcopy ValidateFastaFile.pdb %1 /d /Y)
-if exist %1\ValidateFastaFile.xml (xcopy ValidateFastaFile.xml %1 /d /Y)
+if exist %1\ValidateFastaFile.pdb (xcopy ValidateFastaFile.pdb %1 /d /y)
+if exist %1\ValidateFastaFile.xml (xcopy ValidateFastaFile.xml %1 /d /y)
+if exist %1\FlexibleFileSortUtility.pdb (xcopy FlexibleFileSortUtility.pdb %1 /d /y)
