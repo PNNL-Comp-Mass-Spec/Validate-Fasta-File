53e5d14a3d2b213b5900fb0f454ae269769ec846
alchemistmatt <matthew.monroe@pnnl.gov>
2021-11-20 09:41:58 -0800

    Update batch files to look for .exe

--- ExampleData/ProcessFiles.bat
+++ ExampleData/ProcessFiles.bat
@@ -1,22 +1,40 @@
+@echo off
+
+set ExePath=ValidateFastaFile.exe
+
+if exist %ExePath% goto DoWork
+if exist ..\%ExePath% set ExePath=..\%ExePath% && goto DoWork
+if exist ..\Bin\%ExePath% set ExePath=..\Bin\%ExePath% && goto DoWork
+
+echo Executable not found: %ExePath%
+goto Done
+
+:DoWork
+echo.
+echo Processing with %ExePath%
+echo.
+
 rem Create a fixed fasta file, renaming proteins with duplicate names
-..\bin\ValidateFastaFile.exe JunkTest.fasta /F /R
+%ExePath% JunkTest.fasta /F /R
 
 rem Create a _ProteinHashes.txt file
-..\bin\ValidateFastaFile.exe JunkTest.fasta /B
+%ExePath% JunkTest.fasta /B
 
 rem Create a FastaFileStats .txt file
-..\bin\ValidateFastaFile.exe JunkTest.fasta /C
-..\bin\ValidateFastaFile.exe JunkTest_UTF8.fasta /C
+%ExePath% JunkTest.fasta /C
+%ExePath% JunkTest_UTF8.fasta /C
 
 rem Process a FASTA file with UTF-8 encoding
-..\bin\ValidateFastaFile.exe JunkTest_UTF8.fasta /B /F /R
+%ExePath% JunkTest_UTF8.fasta /B /F /R
 
 rem Process a gzipped FASTA file
-..\bin\ValidateFastaFile.exe JunkTest2.fasta.gz /F /R
+%ExePath% JunkTest2.fasta.gz /F /R
 
-..\bin\ValidateFastaFile.exe Shewanella_2003-12-19.fasta /F
+%ExePath% Shewanella_2003-12-19.fasta /F
 
 rem Use a parameter file to specify options
-..\bin\ValidateFastaFile.exe QC_Standards_2004-01-21_Dup.fasta /P:ValidateFastaFileOptions_GenerateFixedFasta.xml
+%ExePath% QC_Standards_2004-01-21_Dup.fasta /P:ValidateFastaFileOptions_GenerateFixedFasta.xml
+
+:Done
 
 pause
--- ExampleData/StartValidateFastaFile.bat
+++ ExampleData/StartValidateFastaFile.bat
@@ -1,26 +1,48 @@
-..\bin\ValidateFastaFile.exe JunkTest.fasta /P:ValidateFastaFile_ModelSettings.xml
+@echo off
+
+set ExePath=ValidateFastaFile.exe
+
+if exist %ExePath% goto DoWork
+if exist ..\%ExePath% set ExePath=..\%ExePath% && goto DoWork
+if exist ..\Bin\%ExePath% set ExePath=..\Bin\%ExePath% && goto DoWork
+
+echo Executable not found: %ExePath%
+goto Done
+
+:DoWork
+echo.
+echo Processing with %ExePath%
+echo.
+
+%ExePath% JunkTest.fasta /P:ValidateFastaFile_ModelSettings.xml
 @echo off
 echo.
 echo.
 @echo on
 
-..\bin\ValidateFastaFile.exe JunkTest_UTF8.fasta /P:ValidateFastaFile_ModelSettings.xml
+%ExePath% JunkTest_UTF8.fasta /P:ValidateFastaFile_ModelSettings.xml
 @echo off
 echo.
 echo.
 @echo on
 
-..\bin\ValidateFastaFile.exe JunkTest.fasta /P:ValidateFastaFile_CreateStatsFile.xml
-..\bin\ValidateFastaFile.exe JunkTest_UTF8.fasta /P:ValidateFastaFile_CreateStatsFile.xml
+%ExePath% JunkTest.fasta /P:ValidateFastaFile_CreateStatsFile.xml
+%ExePath% JunkTest_UTF8.fasta /P:ValidateFastaFile_CreateStatsFile.xml
 @echo off
 echo.
 echo.
 @echo on
 
-..\bin\ValidateFastaFile.exe QC_Standards_2004-01-21_Dup.fasta /P:ValidateFastaFileOptions_GenerateFixedFasta.xml
+%ExePath% QC_Standards_2004-01-21_Dup.fasta /P:ValidateFastaFileOptions_GenerateFixedFasta.xml
 
 @echo off
 rem Other examples:
 
-rem ..\bin\ValidateFastaFile.exe H_Sapiens_IPI_2009-01-20.fasta /F /D
+rem %ExePath% H_Sapiens_IPI_2009-01-20.fasta /F /D
 rem ValidateFastaFile.exe /I:\\gigasax\dms_organism_Files\*.fasta /p:ValidateFastaFileOptions.xml /c /s:2
+
+
+:Done
+
+pause
+
