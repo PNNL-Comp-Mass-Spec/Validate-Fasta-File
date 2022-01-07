8072a76277ce6c4fcfeb387fbf35ef7ce4ed0c5c
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-07 11:23:18 -0800

    Update PRISM.dll from NuGet

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace ValidateFastaFile
     {
         // Ignore Spelling: isoleucine, leucine, parseable, pre
 
-        public const string PROGRAM_DATE = "November 20, 2021";
+        public const string PROGRAM_DATE = "January 7, 2022";
 
         private static string mInputFilePath;
         private static string mOutputDirectoryPath;
