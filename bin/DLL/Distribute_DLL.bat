19e99a0d02d1f4228b1031aab1dea9d843982c6e
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-26 13:07:53 -0800

    Remove directory from batch file

--- bin/DLL/Distribute_DLL.bat
+++ bin/DLL/Distribute_DLL.bat
@@ -5,7 +5,6 @@ call Distribute_DLL_Work.bat F:\Documents\Projects\DataMining\Protein_Digestion_
 call Distribute_DLL_Work.bat F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\Lib\
 
 call Distribute_DLL_Work.bat F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Debug\
-call Distribute_DLL_Work.bat F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Executables\Release\
 call Distribute_DLL_Work.bat F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Protein_Uploader\bin\
 call Distribute_DLL_Work.bat F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Lib\
 
